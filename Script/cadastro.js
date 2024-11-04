c4748701eb47cafd41b9af907c122c6a4a38fb5b
SOFIA FERREIRA PEREIRA <sofiaferreira.p05@gmail.com>
2024-11-04 17:15:08 -0300

    melhorias na parte de cadastro

--- Script/cadastro.js
+++ Script/cadastro.js
@@ -1,3 +1,33 @@
+const dataNascimento = document.getElementById('input_date');
+dataNascimento.addEventListener("input", function (e) {
+    let value = e.target.value;
+    value = value.replace(/\D/g, "");
+
+    if (value.length >= 5) {
+      value = value.replace(/^(\d{2})(\d{2})(\d{0,4}).*/, "$1/$2/$3");
+    } else if (value.length >= 3) {
+      value = value.replace(/^(\d{2})(\d{0,2})/, "$1/$2");
+    }
+
+    e.target.value = value;
+});
+
+const numeroTelefone = document.getElementById('input_phone_number');
+numeroTelefone.addEventListener("input", function (e) {
+    let value = e.target.value;
+    value = value.replace(/\D/g, ""); 
+
+    if (value.length >= 11) {
+      value = value.replace(/^(\d{2})(\d{5})(\d{0,4}).*/, "$1 $2-$3");
+    } else if (value.length >= 7) {
+      value = value.replace(/^(\d{2})(\d{0,5})/, "$1 $2");
+    } else if (value.length >= 3) {
+      value = value.replace(/^(\d{2})(\d{0,4})/, "$1 $2");
+    }
+
+    e.target.value = value;
+  });
+
 const inputSenha = document.getElementById('input_password');
 const iconSenha = document.getElementById('icon_password');
 iconSenha.addEventListener('click', function() {
